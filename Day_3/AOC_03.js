bd776dec7f5923a3aa86153edda199801dc28ba3
Garance-clt <93951604+Garance-clt@users.noreply.github.com>
2024-12-03 20:09:03 +0000

    day 3

---
+++ Day_3/AOC_03.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+
+// Lire le fichier
+fs.readFile('AOC_ENTRY.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error("Erreur lors de la lecture du fichier :", err);
+        return;
+    }
+
+    // Étape 1 : Somme des multiplications sans conditions
+    const mulRegex = /mul\((\d+),(\d+)\)/g; 
+    const matches = [...data.matchAll(mulRegex)]; 
+
+    let totalSum = matches.reduce((sum, match) => {
+        const a = parseInt(match[1], 10);
+        const b = parseInt(match[2], 10);
+        return sum + a * b;
+    }, 0);
+
+    console.log("Étape 1 : Somme des multiplications (sans conditions) :", totalSum);
+
+    // Étape 2 
+    const doRegex = /do\(\)/; 
+    const dontRegex = /don't\(\)/; 
+
+    let enabled = true; // Les instructions de multiplactions sont activées par défaut
+    let conditionalSum = 0;
+
+    const instructions = data.split(/(?<=\))|(?=do\(\)|don't\(\))/); 
+
+    instructions.forEach(instruction => {
+        if (doRegex.test(instruction)) {
+            enabled = true; 
+        } else if (dontRegex.test(instruction)) {
+            enabled = false;
+        } else {
+            // Vérifier les instructions `mul` uniquement si activées
+            let match;
+            while ((match = mulRegex.exec(instruction)) !== null) {
+                const a = parseInt(match[1], 10);
+                const b = parseInt(match[2], 10);
+                if (enabled) {
+                    conditionalSum += a * b; 
+                }
+            }
+        }
+    });
+
+    console.log("Étape 2 : Somme des multiplications (avec conditions) :", conditionalSum);
+});
